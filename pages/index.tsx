9de456089e6b4f1a81e56b0636ad93ad5e0e8a49
Brent Mitton <brent.mitton+github@gmail.com>
2020-08-15 17:13:52 -0400

    can now get the current status of the bookshelf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import { BlogPreview } from "../components/BlogPreview";
 import Layout from "../components/Layout";
-
-const IndexPage = () => (
+import axios from "axios";
+const IndexPage = ({ processes }: { processes: any }) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <BlogPreview />
+
+    {Object.keys(processes).map((key: string) => {
+      return (
+        <div>
+          NAME: {key} STATUS {processes[key]}
+        </div>
+      );
+    })}
   </Layout>
 );
+
+export async function getServerSideProps() {
+  const apiUrl = process.env.BOOKSHELF_API || "";
+
+  const processes = await axios.get(apiUrl);
+
+  return { props: { processes: processes.data } };
+}
 
 export default IndexPage;
