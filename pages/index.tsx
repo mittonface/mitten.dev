e19e2bec56024056b3e817ccd9ff4387304062a4
Brent Mitton <brent.mitton+github@gmail.com>
2021-01-18 13:45:49 -0500

    print out subdomain

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { BlogPreview } from "../components/BlogPreview";
 import Layout from "../components/Layout";
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <BlogPreview />
-  </Layout>
-);
+const IndexPage = () => {
+  const subdomain = window.location.host.split(".")[1]
+    ? window.location.host.split(".")[0]
+    : false;
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      THE SUBDOMAIN IS ${subdomain}
+      <BlogPreview />
+    </Layout>
+  );
+};
 
 export default IndexPage;
